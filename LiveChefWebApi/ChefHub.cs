cead9baa30c33a70cc10803b9b3e843f84621dcb
Josip Jan <josip.jan@gmail.com>
2017-09-25 13:56:57 +0200

    Included "SignalR" library in the project, and created OWIN class "Startup" required for running the application

---
+++ LiveChefWebApi/ChefHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LiveChefWebApi
+{
+    public class ChefHub : Hub
+    {
+        public void UpdateCooking(int cookingId, string cookingUsername, string cookingDish, string cookingStatus)
+        {
+            Clients.All.broadcastCooking(cookingId, cookingUsername, cookingDish, cookingStatus);
+        }
+    }
+}
