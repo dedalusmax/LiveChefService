ff7e5539af84bb93ced8e35fdf18fb0b797ee63a
dedalusmax <dedalus123@gmail.com>
2017-09-28 10:30:52 +0200

    corrected deploy

--- LiveChefWebApi/Properties/AssemblyInfo.cs
+++ LiveChefWebApi/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LiveChefWebApi")]
+[assembly: AssemblyTitle("LiveChefService")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LiveChefWebApi")]
+[assembly: AssemblyProduct("LiveChefService")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
