de41cce7a8940002a248b0a2c1b5639c05eb557d
dedalusmax <dedalus123@gmail.com>
2017-10-11 10:24:43 +0200

    recording done, snapshots corrected

--- LiveChefWebApi/app/models/cooking-presenter.view-model.js
+++ LiveChefWebApi/app/models/cooking-presenter.view-model.js
@@ -56,17 +56,8 @@
         });
     }
 
-    self.startRecording = function () {
-
-        //communicator.stopLocalStream();
-        //communicator.clear();
-    };
-
-    self.stopRecording = function () {
-
-        //communicator.stopLocalStream();
-        //communicator.clear();
-    };
+    self.mediaRecorder = null;
+    self.recordedBlobs = [];
 
     self.chatMessage = ko.observable('');
 };
@@ -107,7 +98,7 @@ CookingPresenterViewModel.prototype.takeSnapshot = function () {
 
     self.snapshots.push(snapshot);
 
-    var canvas = document.querySelector('#canvas' + snapshot.snapshotId);
+    var canvas = document.querySelector('#' + snapshot.snapshotId);
 
     canvas.getContext('2d').
         drawImage(video, 0, 0, canvas.width, canvas.height);
@@ -138,3 +129,105 @@ CookingPresenterViewModel.prototype.sendChatMessage = function () {
         console.log('Chat message sent to others.');
     });
 }
+
+CookingPresenterViewModel.prototype.startRecording = function () {
+    var self = this;
+
+    self.recordedBlobs = [];
+
+    var mediaSource = new MediaSource();
+    mediaSource.addEventListener('sourceopen', self.handleSourceOpen, false);
+
+    var communicator = root.main().communicator;
+
+    var options = { mimeType: 'video/webm;codecs=vp9' };
+    if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+        console.log(options.mimeType + ' is not Supported');
+        options = { mimeType: 'video/webm;codecs=vp8' };
+        if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+            console.log(options.mimeType + ' is not Supported');
+            options = { mimeType: 'video/webm' };
+            if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+                console.log(options.mimeType + ' is not Supported');
+                options = { mimeType: '' };
+            }
+        }
+    }
+    try {
+        self.mediaRecorder = new MediaRecorder(communicator.localStream, options);
+    } catch (e) {
+        console.error('Exception while creating MediaRecorder: ' + e);
+        alert('Exception while creating MediaRecorder: '
+            + e + '. mimeType: ' + options.mimeType);
+        return;
+    }
+    console.log('Created MediaRecorder', self.mediaRecorder, 'with options', options);
+
+    self.mediaRecorder.start(10); // collect 10ms of data
+    console.log('MediaRecorder started', self.mediaRecorder);
+    self.isRecording(true);
+
+    self.mediaRecorder.onstop = self.handleStop.bind(self);
+    self.mediaRecorder.addEventListener('dataavailable', self.handleDataAvailable.bind(self, event));
+};
+
+CookingPresenterViewModel.prototype.handleDataAvailable = function (e) {
+    var self = this;
+
+    if (event.data && event.data.size > 0) {
+        self.recordedBlobs.push(event.data);
+    }
+    console.log('Recorded Blobs: ', self.recordedBlobs);
+}
+
+CookingPresenterViewModel.prototype.stopRecording = function () {
+    var self = this;
+
+    self.mediaRecorder.stop();
+    console.log('Recorded Blobs: ', self.recordedBlobs);
+}
+
+CookingPresenterViewModel.prototype.handleStop = function () {
+    var self = this;
+
+    console.log('Recorder stopped');
+    self.isRecording(false);
+}
+
+CookingPresenterViewModel.prototype.playRecordedVideo = function () {
+    var self = this;
+
+    var superBuffer = new Blob(self.recordedBlobs, { type: 'video/webm' });
+    var recordedVideo = document.querySelector('#recordedVideo');
+    recordedVideo.src = window.URL.createObjectURL(superBuffer);
+}
+
+CookingPresenterViewModel.prototype.downloadVideo = function () {
+    var self = this;
+
+    var blob = new Blob(self.recordedBlobs, { type: 'video/webm' });
+    var url = window.URL.createObjectURL(blob);
+
+    var downloadAnchor = document.querySelector('a#downloadVideo');
+
+    // clear the anchor tag of previous transfer
+    downloadAnchor.removeAttribute('download');
+    if (downloadAnchor.href) {
+        URL.revokeObjectURL(downloadAnchor.href);
+        downloadAnchor.removeAttribute('href');
+    }
+
+    downloadAnchor.href = url;
+    downloadAnchor.download = 'chefRecord.webm';
+    downloadAnchor.style.display = 'none';
+    downloadAnchor.click();
+}
+
+CookingPresenterViewModel.prototype.handleSourceOpen = function () {
+    var self = this;
+
+    console.log('MediaSource opened');
+    self.sourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="vp8"');
+    console.log('Source buffer: ', self.sourceBuffer);
+}
+
